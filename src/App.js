409ac94a0ac7b000ed800a470a69081ba977f1f4
pyrobit <3159587+pyrobit@users.noreply.github.com>
2020-07-06 10:14:20 -0300

    tests valid response

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+        setRepository(response.data);
+        //console.log(response);
+    })
+},[]);  
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      
+      title: "Novo Repo ReactJS",
+      url: "https://github.com/pyrobit/challenge-concepts-react.git",
+      techs: ["React", "Node.js"]
+      
+    });
+
+    const repo = response.data;
+    setRepository( [...repositories, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepository(repositories.filter(repo => repo.id !== id ));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        
+        {repositories.map(repo => (<li key={repo.id}>
+          {repo.title} 
+          <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+          </li>)
+          )}
+  
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>
+        Adicionar
+        </button>
     </div>
   );
 }
